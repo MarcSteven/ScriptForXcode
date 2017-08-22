938f5759f5468f0c6a61dbb7413270d9717a3d91
Marc Steven <MarcSteven@users.noreply.github.com>
2017-08-22 17:47:05 +0800

    Update Fix_To_Do_Warning.sh
    Modify the error

--- Fix_To_Do_Warning.sh
+++ Fix_To_Do_Warning.sh
@@ -6,4 +6,4 @@
 #  Created by Marc Zhao on 2017/5/6.
 #
 TAGS="TODO:|FIXME:
-find "${SRCROOT}" \( -type f -name "*.m" | - name ".swift"\) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
+find "${SRCROOT}" \( -type f  - name ".swift"\) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
