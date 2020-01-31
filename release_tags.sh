9c2ee4c18ed39361aa0993296353e45ac8bc6c8d
Marc Steven <MarcSteven@users.noreply.github.com>
2020-01-31 21:50:24 +0800

    remove the unused one

--- release_tags.sh
+++ release_tags.sh
@@ -3,4 +3,4 @@ VERSION=`grep VERSION setup.py | head -1 | sed 's/.*"\(.*\)".*/\1/'`
 git tag $VERSION
 git push origin $VERSION”
 
-Excerpt From: Akos Hochrein. “Designing Microservices with Django.” Apple Books. 
+
