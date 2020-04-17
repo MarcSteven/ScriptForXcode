45eaba3f57852e3e68daa638d4323db8fa5d029d
Marc Steven <MarcSteven@users.noreply.github.com>
2020-04-17 09:46:24 +0800

    Update merge_framework.rb

--- merge_framework.rb
+++ merge_framework.rb
@@ -1,5 +1,25 @@
 #!/usr/bin/env ruby
-
+def build_target(target,sdk,build_dir,archs,misc ='')
+  command = <<-EOC.strip.gsub(/^[\t]+/,'')
+  xcodebuild \
+  -project #{project_path} \
+  -target #{target_name} \
+  -configuration #{configuration}
+  EOC
+  command += "-sdk #{sdk}" unless empty_string?(sdk)
+  command += "CONFIGURATION_BUILD_DIR = \"#{build_Dir}\"" unless empty_string?(build_dir)
+  command += " ARCHS=\"#{archs}\"" unless empty_string?(archs)
+  comand  += " #{misc}" unless empty_string?(misc)
+  execmd command
+end
+def build_ios_sdk(target_name)
+  clean_target target_name
+  prefix = File.join(slice_build_path,target_name)
+  build_target target_name, 'iphonesimulator', File.join(prefix,'iphonesimulator'),'i386 x86_64'
+  build_target target_name, 'iphoneos', File.join(prefix,'iphones'),'armv7'
+  
+  
+end
 
 def merge_framework(prefix,target_name)
   product_name = product_name(target_name)
